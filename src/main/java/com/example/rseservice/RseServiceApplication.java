b84bfbc55f36d1e9761d65c006fd112fe253ea84
Lucas Gontijo <lucasgontijo0@gmail.com>
2018-11-07 14:00:41 -0200

    First commit!

---
+++ src/main/java/com/example/rseservice/RseServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.rseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RseServiceApplication.class, args);
+    }
+}
